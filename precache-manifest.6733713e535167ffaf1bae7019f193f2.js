5ce535ea1396975f931b7191cb3ac588e42b6a09
JonathanRamirez <jonajramirez@gmail.com>
2019-11-12 01:41:49 -0600

    tool: First Template github io

---
+++ precache-manifest.6733713e535167ffaf1bae7019f193f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bc7c67a3e403523ae1ee69da3ce921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a834e7d077f775e3272d",
+    "url": "/static/css/main.a8eda805.chunk.css"
+  },
+  {
+    "revision": "cec55184fa54e0fc95e2",
+    "url": "/static/js/2.e82515e0.chunk.js"
+  },
+  {
+    "revision": "a834e7d077f775e3272d",
+    "url": "/static/js/main.c2fa6d19.chunk.js"
+  },
+  {
+    "revision": "a377f9ef93891f5646d5",
+    "url": "/static/js/runtime-main.48cf3704.js"
+  },
+  {
+    "revision": "3df12d895c4875838c3de7904c2631c3",
+    "url": "/static/media/header.3df12d89.jpg"
+  },
+  {
+    "revision": "dd45cb1eb2a3ed2b00c936657b45d30b",
+    "url": "/static/media/photo.dd45cb1e.png"
+  }
+]);
